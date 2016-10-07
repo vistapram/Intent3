3c55d927e8ab417c759481c5cc453d615600705d
vistapram <vistapramudya@gmail.com>
2016-10-07 10:01:36 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -20,6 +20,20 @@ public class MainActivity extends AppCompatActivity {
                         dialPhoneNumber("0341712500");
                     }
                 });
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            composeSmsMessage("Pesan Dari SMK Telkom Malang");
+            }
+        });
+    }
+
+    public void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     public void dialPhoneNumber(String phoneNumber) {
@@ -28,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
+
+
 
 
 }
